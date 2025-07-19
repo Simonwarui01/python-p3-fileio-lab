c6b54d0ecd0716a6f1ef80de3e83a6fe7ed471f9
Simon <simonwarui93@gmail.com>
2025-07-20 01:11:39 +0300

    assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    # Add .txt extension
+    txt_file = file_name.with_suffix(".txt")
+    with open(txt_file, "w") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Add .txt extension
+    txt_file = file_name.with_suffix(".txt")
+    with open(txt_file, "a") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    # Add .txt extension
+    txt_file = file_name.with_suffix(".txt")
+    with open(txt_file, "r") as f:
+        return f.read()
